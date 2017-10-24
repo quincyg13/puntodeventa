5b5c5d1d891a6975cf832d7593c78c18e1cab002
Gilberth <quincyg13@gmail.com>
2017-10-23 19:45:40 -0500

    add agregar producto(Liz)
    Liz agrego la opción de añadir producto

--- InterfazBasica/InterfazBasica/InterfazBasica/src/InterfazBasica/Principal.java
+++ InterfazBasica/InterfazBasica/InterfazBasica/src/InterfazBasica/Principal.java
@@ -7,38 +7,35 @@ package InterfazBasica;
 
 import javax.swing.*;
 
-
 /**
  *
  * @author ishrael
  */
 public class Principal {
-      
 
- 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   
+
         JFrame principal = new JFrame("Punto de venta");
         ModeloTablaVenta mtmv = new ModeloTablaVenta();
         ModeloTablaProducto mtmp = new ModeloTablaProducto();
-        PanelPrincipal panelprincipal = new PanelPrincipal(mtmv,mtmp); 
-        BarraMenu menu = new BarraMenu();
-        OyentePrincipal oyente =new OyentePrincipal(panelprincipal);
-        
+        PanelPrincipal panelprincipal = new PanelPrincipal(mtmv, mtmp);
+        BarraMenu menu = new BarraMenu(mtmp);
+        OyentePrincipal oyente = new OyentePrincipal(panelprincipal);
+
         OyenteMenu oyenteMenu = new OyenteMenu(menu);
         panelprincipal.addEventos(oyente);
         //menu.addEventos(oyenteMenu);
-       
-        principal.setSize(650,350);
+
+        principal.setSize(650, 350);
         principal.setResizable(false);
-        principal.setLocation(100,100);
+        principal.setLocation(100, 100);
         principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         principal.setJMenuBar(menu.createMenuBar());
         principal.setContentPane(panelprincipal);
         principal.setVisible(true);
     }
-    
+
 }
